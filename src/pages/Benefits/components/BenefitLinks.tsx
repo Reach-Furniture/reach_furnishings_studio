5b4abee4c98a70d97c8c9de019cb13c454c912db
Rajshree S <raj02shende@gmail.com>
2026-02-06 16:02:54 +0530

    Initial commit

--- src/pages/Benefits/components/BenefitLinks.tsx
+++ src/pages/Benefits/components/BenefitLinks.tsx
@@ -18,7 +18,7 @@ const BenefitLinks = () => {
         </h2>
 
         <p className="text-center text-lg text-gray-600 max-w-3xl mx-auto mb-12">
-          Furnishings Studio
+          Best In Class Furnishings Studio Software
         </p>
 
 
